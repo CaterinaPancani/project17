d81edd3ff73d735ad92127606533dbe230336c45
Kate <pancani.caterina@gmail.com>
2024-03-15 19:35:56 +0100

    completata creazione utente, classe vista e classe form con livewire

--- app/Livewire/Users/Create.php
+++ app/Livewire/Users/Create.php
@@ -8,12 +8,13 @@ use Livewire\Attributes\Validate;
 
 class Create extends Component
 {
-
+    public UserForm $form;
     public function render()
     {
         return view('livewire.users.create');
     }
     public function store(){
+        $this->form->save();
         session()->flash('success','user successfully created.');
         $this->reset();
     }
--- resources/views/livewire/users/create.blade.php
+++ resources/views/livewire/users/create.blade.php
@@ -4,21 +4,21 @@
 <form wire:submit.prevent="store">
     <div class="mb-3">
       <label for="exampleInputPassword1" class="form-label">Name</label>
-      <input type="text" class="form-control" wire:model.blur="name">
+      <input type="text" class="form-control" wire:model.change="form.name">
       @error('name')
       {{$message}}
       @enderror
     </div>
     <div class="mb-3">
       <label for="exampleInputEmail1" class="form-label">Email address</label>
-      <input type="email" class="form-control" wire:model.blur="email">
+      <input type="email" class="form-control" wire:model.change="form.email">
       @error('email')
       {{$message}}
       @enderror
     </div>
     <div class="mb-3">
         <label for="exampleInputPassword1" class="form-label">Password</label>
-        <input type="password" class="form-control" wire:model="password">
+        <input type="password" class="form-control" wire:model.change="form.password">
         @error('password')
         {{$message}}
         @enderror
